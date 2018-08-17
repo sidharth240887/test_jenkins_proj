e24a824184eb65daffa321cce1075c1604ea96bf
Sidharth Chaudhary <sidharth.chaudhary@mel.setec.com.au>
2018-08-17 11:55:20 +1000

    first commit

---
+++ _build/CMakeFiles/prog.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/prog.dir/Main.cpp.obj"
+  "prog.pdb"
+  "prog.exe"
+  "prog.exe.manifest"
+  "libprog.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/prog.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
